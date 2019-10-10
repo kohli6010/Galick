b769e5638521e5428b314545e6253193eeb22f8d
Rahul <kohli6010@gmail.com>
2019-10-10 19:42:26 +0530

    Added navbar and hoverComponent

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -2,8 +2,31 @@ import React from 'react';
 import './navbar.styles.css';
 
 class Nav extends React.Component{
-	constructor(){
-		
+	constructor(props){
+		super(props);
+		this.state = {
+			isHidden: true
+		}
+	}
+
+	handleClick = () => { 
+		this.setState({isHidden: !this.state.isHidden})
+	}
+
+	render() { 
+		return (
+			<div className="navbar">
+				<div className="logo">Safety Squad</div>
+				<div className="left">
+					<span className="home">Home</span>
+					<span className="about">About</span>
+					<span className="checksafety">Check Safety</span>
+					<div className="animation">
+						<span className="dots" onClick={this.handleClick}>...</span>
+					</div>
+				</div>
+			</div>
+		);
 	}
 }
 
